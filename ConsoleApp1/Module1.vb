8e39b918843e14b16e0b34616f93d04d8e812a4f
Michael Hiliger <mkhiliger@gmail.com>
2017-07-01 15:57:37 -0400

    revert

--- ConsoleApp1/Module1.vb
+++ ConsoleApp1/Module1.vb
@@ -5,9 +5,5 @@
     End Sub
 
     'This is the initial save
-    'This is the first save after initial
-    'This is the second save after inital -- did not check this in
-    'This is the third save after initial
-    'This is the fourth after pressing branch
 
 End Module
