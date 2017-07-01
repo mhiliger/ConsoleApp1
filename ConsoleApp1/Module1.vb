251780dcc1ea0fc5214e624b9d2a6f9fbf55e91e
Michael Hiliger <mkhiliger@gmail.com>
2017-07-01 15:41:37 -0400

    Third change

--- ConsoleApp1/Module1.vb
+++ ConsoleApp1/Module1.vb
@@ -6,6 +6,7 @@
 
     'This is the initial save
     'This is the first save after initial
-    'This is the second save after inital
+    'This is the second save after inital -- did not check this in
+    'This is the third save after initial
 
 End Module
